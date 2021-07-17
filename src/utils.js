bffe1ffac0869beb0838d7748d85da522309c6ce
minhle <leeminh91@gmail.com>
2021-07-17 11:34:38 +0700

    - update

--- src/utils.js
+++ src/utils.js
@@ -9,16 +9,31 @@ export const firebaseConnect = (collection) => {
                 const data = query.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
                 cb && cb(data);
             }),
-        setItem: (item) => connectDB.add(item),
-        deleteItem: (id) => connectDB.doc(id).delete(),
-        updateItem: (id, item) => connectDB.doc(id).update(item),
-        updateAll: (valueUpdate, cb = null) =>
+        setItem: (item, cb = null) =>
+            connectDB.add(item).then(() => {
+                cb && cb();
+            }),
+        deleteItem: (id, cb = null) =>
+            connectDB
+                .doc(id)
+                .delete()
+                .then(() => {
+                    cb && cb();
+                }),
+        updateItem: (id, item, cb = null) =>
+            connectDB
+                .doc(id)
+                .update(item)
+                .then(() => {
+                    cb && cb();
+                }),
+        updateAll: (item, cb = null) =>
             connectDB
                 .get()
                 .then((query) => {
                     const newListUpdate = [];
                     query.docs.map((doc) => {
-                        newListUpdate.push(doc.ref.update(valueUpdate));
+                        newListUpdate.push(doc.ref.update(item));
                     });
                     return Promise.all(newListUpdate);
                 })
